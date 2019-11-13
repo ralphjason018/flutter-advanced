4982658edfb0dd12a2441f10b1e46b2635e11105
mjsolidarios <bashdev7@gmail.com>
2019-11-14 00:25:38 +0800

    Attempt to fix port

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 //end body-parser configuration
 
 //create app server
-var server = app.listen(3000,  "127.0.0.1", function () {
-
+var server = app.listen(process.env.PORT || 5000, function () {
   var host = server.address().address
   var port = server.address().port
 
